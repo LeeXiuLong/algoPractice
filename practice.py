176c04af04eba90c78faf6d0dcb74c9a747a669c
Jourdan Ooi <jungterooi@gmail.com>
2021-01-18 13:56:28 -0500

    new comment from windows

--- practice.py
+++ practice.py
@@ -1,32 +1,34 @@
 def smallestDifference(arrayOne, arrayTwo):
-	arrayOne.sort()
-	arrayTwo.sort()
-	pointer1 = 0
-	pointer2 = 0
-	smallest = float("inf")
-	current = 0
-	returnArr = []
-	
-	while pointer1 < len(arrayOne) and pointer2 < len(arrayTwo):
-		current1 = arrayOne[pointer1]
-		current2 = arrayTwo[pointer2]
-		current = abs(current1 - current2)
-		if current1 == current2:
-			return [current1, current2]
-		if current < smallest:
-			smallest = current
-			returnArr = [current1, current2]
-		if current1 < current2:
-			pointer1 += 1
-		elif current2 < current1:
-			pointer2 += 1
-	return returnArr
+    arrayOne.sort()
+    arrayTwo.sort()
+    pointer1 = 0
+    pointer2 = 0
+    smallest = float("inf")
+    current = 0
+    returnArr = []
+
+    while pointer1 < len(arrayOne) and pointer2 < len(arrayTwo):
+        current1 = arrayOne[pointer1]
+        current2 = arrayTwo[pointer2]
+        current = abs(current1 - current2)
+        if current1 == current2:
+            return [current1, current2]
+        if current < smallest:
+            smallest = current
+            returnArr = [current1, current2]
+        if current1 < current2:
+            pointer1 += 1
+        elif current2 < current1:
+            pointer2 += 1
+    return returnArr
 
 
 def moveElementToEnd(array, toMove):
-	currentPlace = 0
-	for i in range(len(array)):
-		if array[i] != toMove:
-			array[i], array[currentPlace] = array[currentPlace], array[i]
-			currentPlace += 1
-	return array
+    currentPlace = 0
+    for i in range(len(array)):
+        if array[i] != toMove:
+            array[i], array[currentPlace] = array[currentPlace], array[i]
+            currentPlace += 1
+    return array
+
+# this is a comment
