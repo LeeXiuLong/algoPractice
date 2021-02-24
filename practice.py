b08ee3e4a6bbeed51f1ba4704c0ba8db16a9dffd
Jourdan Ooi <jungterooi@gmail.com>
2021-02-24 12:52:58 -0500

    productSum problem

--- practice.py
+++ practice.py
@@ -50,6 +50,15 @@ def depthFirstSearch(self, array):
 			child.depthFirstSearch(array)
 	return array
 
+def getNthFib(n):
+    if n == 1:
+        return 0
+    if n == 2: 
+        return 1
+    else:
+        return getNthFib(n-1) + getNthFib(n-2)
+
+
 
         
     
