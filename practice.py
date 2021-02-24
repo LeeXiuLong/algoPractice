9cb6a9526e5fdb3ceefc938010d59f585e7d017c
Jourdan Ooi <jungterooi@gmail.com>
2021-02-24 13:33:28 -0500

    finished bubble sort algo

--- practice.py
+++ practice.py
@@ -98,6 +98,16 @@ def findThreeLargestNumbers(array):
             smallest = number
     
     return [smallest, middle, largest]
+
+def bubbleSort(array):
+    sorted = False
+    while not sorted:
+        sorted = True
+        for i in range(len(array)-1):
+            if array[i] > array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+                sorted = False
+    return array
          
 
         
